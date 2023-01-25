ade252013ce6c036070dcdec04817c853b768918
Flrande <1978616327@qq.com>
2023-01-25 15:58:17 +0800

    chore: update demo style

--- packages/playground/src/Editor.tsx
+++ packages/playground/src/Editor.tsx
@@ -84,7 +84,7 @@ export const Editor = () => {
     <div className={'grid grid-cols-2 gap-4 h-full w-full'}>
       <div
         className={
-          'grid grid-rows-[40px_repeat(3,minmax(0,1fr))] max-h-full overflow-scroll'
+          'grid grid-rows-[40px_40px_repeat(2,minmax(0,1fr))] max-h-full overflow-scroll'
         }
       >
         <ToolBar editor={editorA} undoManager={undoManagerA}></ToolBar>
@@ -101,7 +101,7 @@ export const Editor = () => {
       </div>
       <div
         className={
-          'grid grid-rows-[40px_repeat(3,minmax(0,1fr))] max-h-full overflow-scroll'
+          'grid grid-rows-[40px_40px_repeat(2,minmax(0,1fr))] max-h-full overflow-scroll'
         }
       >
         <ToolBar editor={editorB} undoManager={undoManagerB}></ToolBar>
